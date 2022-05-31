da2fa935d703cd87096e75ad8b8dc5244942848a
2019147547 <101635621+2019147547@users.noreply.github.com>
2022-06-01 01:47:58 +0900

    Delete LAB5 directory

--- LAB5/script.js
+++
@@ -1,9 +0,0 @@
-fetch('product.json')
-.then(response => {
-    if (!response.ok) {
-        throw new Error();
-    }
-    return response.json();
-})
-.then(json => initialize(json))
-.catch(err => console.error());
